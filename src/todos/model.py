cfd987e1539260debd400792aa0e984d5bee5d6b
saras shrestha <sarasstha5@gmail.com>
2026-09-24 22:05:44 +0545

    added relationships between tables

--- src/todos/model.py
+++ src/todos/model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String,Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String,Boolean
+from sqlalchemy.orm import relationship
 from src.db.database import Base
 
 class Todo(Base):
@@ -8,3 +9,5 @@ class Todo(Base):
     Title = Column(String, nullable=False)
     Description = Column(String, nullable=False)
     is_complete = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False) # Foreign key to the User table
+    user = relationship("User", back_populates="todos") # Relationship to the User table, can access the user associated with the todo item
--- src/users/model.py
+++ src/users/model.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import relationship
 from src.db.database import Base
 
 class User(Base):
-    __tablename__ = "User"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     hashed_password = Column(String, nullable=False)
+    todos = relationship("Todo", back_populates="user") # Relationship to the Todo table, can access all todo items associated with the user
